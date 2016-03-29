77fe9fd85675891c7b4c3a222dfd4adc8ac5b542
andrei nikolaev <andrei.nikolaew@gmail.com>
2016-03-29 17:41:53 +0300

    refactoring providers list controller

--- VTraktate/app/providers/providers.ctrl.js
+++ VTraktate/app/providers/providers.ctrl.js
@@ -1,207 +1,245 @@
-﻿angular.module('app')
-.controller('providersCtrl', function ($q, $scope, $timeout, $resource, $http, ngTableParams, $state, $modal,notifyClient, handleHttpError,
-    GlobalsService, LocalStorageService, EmploymentService, FreelanceService, ProviderTypeService, AvailabilityService, ActionAuthorizationService) {
-    var Api = $resource('/api/provider/:id', { id: '@id' });
+﻿angular.module("app")
+    .factory("providers", ($resource) => {
 
-    $scope.filtersVisible = true;
+        var Provider = $resource("/api/provider/:id", { id: "@id" });
 
-    $scope.toggleAdditionalFilter = function () {
-        $scope.showAdditionalFilter = !$scope.showAdditionalFilter;
-    };
-
-    $scope.deleteItem = function(item) {
-        if (confirm(`Вы действительно хотите удалить ${item.name}?`)) {
-            Api.delete({ id: item.id }).$promise
-                .then(() => {
-                    notifyClient.success(`Данные о ${item.name} удалены`);
-                    $scope.reloadTable();
-                        if ($scope.selectedItemId === item.id)
-                            $scope.clearSelection();
-                    },
-                handleHttpError(`Ошибка при удалении ${item.name}`));
+        return {
+            "delete": deleteProvider
         }
-    }
 
-    $scope.clearSelection = () => {
-        $scope.select(null);
-        if($state.current.name === "hr.employees.profile") 
-            $state.go($state.current, {id: null}, {reload: true});
-    }
-    $scope.showAdditionalFilter = false;
+        function deleteProvider(id) {
+            return Provider.delete({ id: id }).$promise;
+        }
+    })
+    .factory("providerFilterCache", (LocalStorageService, notifyClient) => {
+        var MYFILTERSETTINGS_KEY = "my_filter_settings";
+        return {
+            save,
+            load,
+            clear
+        };
 
-    $scope.toggleFilters = function (value) {
-        $scope.filtersVisible = !value;
-    }
+        function save(settings) {
+            LocalStorageService.set(MYFILTERSETTINGS_KEY, settings);
+            notifyClient.info("Настройки фильтра сохранены.");
+        }
 
-    $scope.selectedItemId = null;
-    $scope.readonly = !ActionAuthorizationService.isHR();
+        function load() {
+            var settings = LocalStorageService.get(MYFILTERSETTINGS_KEY) || {};
+            notifyClient.info("Настройки фильтра восстановлены.");
+            return settings;
+        }
 
-    var types = GlobalsService.get('providerTypes'),
-        tripleChoice = GlobalsService.get('tripleChoice');
+        function clear() {
+            LocalStorageService.remove(MYFILTERSETTINGS_KEY);
+        }
+    })
+    .controller("providersCtrl", function($q, $scope, $timeout, providers, providerFilterCache, $http, ngTableParams, $state, $modal, notifyClient, handleHttpError,
+        GlobalsService, EmploymentService, FreelanceService, ProviderTypeService, AvailabilityService, ActionAuthorizationService) {
 
-    $scope.lists = {
-        providerTypes: types,
-        tripleChoice: tripleChoice
-    };
+        // what is this doing?
+        $scope.test = [];
 
-    $scope.data = {
-        employee: types[0],    // ATTN : MAGIC NUMBER
-        freelancer: types[1],   // ATTN : MAGIC NUMBER
-        contractor: types[2]    // ATTN : MAGIC NUMBER
-    };
+        initState();
 
-    $scope.includeInhouse = true; // filter contains inHouse 
+        $scope.toggleAdditionalFilter = toggleAdditionalFilter;
+        $scope.toggleFilters = toggleFilters;
+        $scope.deleteItem = deleteItem;
+        $scope.clearSelection = clearSelection;
+        // deprecated?
+        $scope.setIncludeInhouse = setIncludeInhouse;
 
-    // deprecated?
-    $scope.setIncludeInhouse = function (values) {
-        $scope.includeInhouse = Array.prototype.indexOf.call(value, 0) >= 0;
-    };
-    // todo: make this personalized !!!! 
-    var MYFILTERSETTINGS_KEY = 'my_filter_settinsg'
+        $scope.saveFilterSettings = saveFilterSettings;
+        // fn name collision!
+        $scope.loadFilterSettings = loadFilterSettings_;
+        // fn name collision!
+        $scope.clearFilterSettings = clearFilterSettings_;
 
-    $scope.saveFilterSettings = function () {
-        var filterSettings = $scope.tableParams.$params.filter;
-        LocalStorageService.set(MYFILTERSETTINGS_KEY, filterSettings);
-        toastr.info('Настройки фильтра сохранены.');
-    }
+        $scope.select = select;
+        $scope.reloadTable = reloadTable;
+        $scope.changeFreelance = changeFreelance;
+        $scope.changeEmployment = changeEmployment;
+        $scope.changeType = changeType;
+        $scope.isEmployee = isEmployee;
+        $scope.showAvailabilityCalendar = showAvailabilityCalendar;
 
-    $scope.loadFilterSettings = function () {
-        var settings = LocalStorageService.get(MYFILTERSETTINGS_KEY);
-        $scope.tableParams.$params.filter = settings || {};
-        toastr.info('Настройки фильтра восстановлены.');
-    }
 
-    function loadFilterSettings() {
-        var result = LocalStorageService.get(MYFILTERSETTINGS_KEY) || clearFilterSettings();
-        return result;
-    }
+        function initState() {
+            $scope.filtersVisible = true;
+            $scope.showAdditionalFilter = false;
+            $scope.selectedItemId = null;
+            $scope.readonly = !ActionAuthorizationService.isHR();
+            var types = GlobalsService.get("providerTypes"),
+                tripleChoice = GlobalsService.get("tripleChoice");
 
-    var tableSettings = $state.current.data.tableDefaults || {};
+            $scope.lists = {
+                providerTypes: types,
+                tripleChoice: tripleChoice
+            };
 
-    function clearFilterSettings() {
-        LocalStorageService.remove(MYFILTERSETTINGS_KEY);
-    }
+            $scope.data = {
+                employee: types[0], // ATTN : MAGIC NUMBER
+                freelancer: types[1], // ATTN : MAGIC NUMBER
+                contractor: types[2] // ATTN : MAGIC NUMBER
+            };
 
-    $scope.clearFilterSettings = function () {
-        $scope.tableParams.filter({});
+            $scope.includeInhouse = true; // filter contains inHouse     
+        }
 
-        toastr.info('Настройки фильтра сброшены. Совсем...');
-    }
-
-    $scope.test = [];
-
-    setTableParams();
-
-    function setTableParams() {
-        $scope.tableParams = $state.current.data.tableParams || new ngTableParams({
-            page: tableSettings.page || 1,            // show first page
-            count: tableSettings.count || 12,          // count per page
-            sorting: tableSettings.sorting || {
-                name: 'asc'     // initial sorting
-            },
-            filter: tableSettings.filter || loadFilterSettings() || {}
-        }, {
-            total: tableSettings.total || 0,           // length of data
-            counts: [12, 24, 50, 100],
-            getData: function ($defer, params) {
-
-                console.log(params.filter());
-                console.log('params');
-                console.log(params);
-                console.log(params.count() + ' ' + params.page());
-
-                //var requestParams = {
-                //    count: params.count(),
-                //    page: params.page(),
-                //    filter: params.filter(),
-                //    sorting : params.sorting() || { name : 'asc' }
-                //};
-
-                Api.get(params.url(), function (data) {
-                    // update table params
-                    params.total(data.result.total);
-                    // set new data
-
-                    $scope.showRateAndQA = false;
-
-                    $scope.showRateAndQA = data.showRateAndQA;
-                    //$scope.$apply(); // otherwise there may be no update of UI 
-
-                    // toggle visibility of columns 
-                    var providerTypes = $scope.tableParams.$params.filter.providerTypes || [];
-
-                    // todo: simplify!!! 
-                    $scope.includeInhouse = providerTypes.indexOf(0) >= 0 || providerTypes.length == 0;
-
-                    if ($scope.tableParams.$params.filter && $scope.tableParams.$params.filter.name !== undefined)
-                        $scope.toggleFilters($scope.tableParams.$params.filter.name);
-
-                    $defer.resolve(data.result.result);
-                });
+        function clearSelection() {
+            if ($scope.selectedItemId === item.id) {
+                $scope.select(null);
+                if ($state.current.name === "hr.employees.profile")
+                    $state.go($state.current, { id: null }, { reload: true });
             }
-        });
-    }
+        }
 
-    $state.current.data.tableParams = $scope.tableParams;
+        function toggleAdditionalFilter() {
+            $scope.showAdditionalFilter = !$scope.showAdditionalFilter;
+        };
 
-    $scope.select = function (itemId) {
-        $scope.selectedItemId = itemId;
-    }
-    $scope.reloadTable = function () {
-        $scope.tableParams.reload();
-    };
 
-    $scope.changeFreelance = function (item) {
-        FreelanceService.changeFreelance(item)
-        .then(function (freelance) {
-            item.freelance = freelance;
-            $scope.tableParams.reload();
-        })
-    }
+        function deleteItem(item) {
+            if (confirm(`Вы действительно хотите удалить ${item.name}?`)) {
+                providers.delete(id)
+                    .then(
+                        afterDelete,
+                        handleHttpError(`Ошибка при удалении ${item.name}`)
+                    );
+            }
 
-    $scope.changeEmployment = function (item) {
-        EmploymentService.changeEmployment(item)
-            .then(function (employment) {
-                item.employment = employment;
-                $scope.tableParams.reload();
-            })
-    }
+            function afterDelete() {
+                notifyClient.success(`Данные о ${item.name} удалены`);
+                $scope.reloadTable();
+                clearSelection();
+            }
+        }
 
-    $scope.changeType = function (item, typeId) {
-        ProviderTypeService
-            .changeType(item, typeId)
-            .then(reloadTable);
-    }
 
-    function reloadTable() {
-        $scope.tableParams.reload();
-    }
+        function toggleFilters(value) {
+            $scope.filtersVisible = !value;
+        }
 
-    $scope.isEmployee = function (item) {
-        return item.typeId === 0 && item.employment;
-    }
 
-    $scope.showAvailabilityCalendar = function (item) {
-        AvailabilityService.getAvailability(item.id)
-            .then(success, failure);
+        // deprecated?
+        function setIncludeInhouse(values) {
+            $scope.includeInhouse = Array.prototype.indexOf.call(value, 0) >= 0;
+        }
 
-        function success(eventsource) {
-            var modalInstance = $modal.open({
-                size: 'lg',
-                resolve: {
-                    eventsource: function () { return eventsource; },
-                    providerId: function () { return item.id; },
+        function saveFilterSettings() {
+            providerFilterCache.save($scope.tableParams.$params.filter);
+        }
 
-                },
-                controller: 'ProviderAvailabilityCalendarController',
-                templateUrl: 'app/providers/availability/availabilityCalendar/availabilityCalendar.dlg.tpl.html'
+        function loadFilterSettings() {
+            $scope.tableParams.$params.filter = providerFilterCache.load();
+        }
+
+        var tableSettings = $state.current.data.tableDefaults || {};
+
+        setTableParams();
+
+        function loadProviderData($defer, params) {
+
+            // TODO: this is an URL and I'm expecting smth else ... 
+            Api.get(params.url(), function(data) {
+                // update table params
+                params.total(data.result.total);
+                // set new data
+
+                $scope.showRateAndQA = false;
+
+                $scope.showRateAndQA = data.showRateAndQA;
+                //$scope.$apply(); // otherwise there may be no update of UI 
+
+                // toggle visibility of columns 
+                var providerTypes = $scope.tableParams.$params.filter.providerTypes || [];
+
+                // todo: simplify!!! 
+                $scope.includeInhouse = providerTypes.indexOf(0) >= 0 || providerTypes.length == 0;
+
+                if ($scope.tableParams.$params.filter && $scope.tableParams.$params.filter.name !== undefined)
+                    $scope.toggleFilters($scope.tableParams.$params.filter.name);
+
+                $defer.resolve(data.result.result);
             });
-
-            modalInstance.result.then(reloadTable, reloadTable);
         }
 
-        function failure(data) {
-            toastr.error('Я ошипко');
+
+        function setTableParams() {
+            $scope.tableParams = $state.current.data.tableParams || new ngTableParams({
+                page: tableSettings.page || 1, // show first page
+                count: tableSettings.count || 12, // count per page
+                sorting: tableSettings.sorting || {
+                    name: "asc" // initial sorting
+                },
+                filter: tableSettings.filter || loadFilterSettings() || {}
+            }, {
+                total: tableSettings.total || 0, // length of data
+                counts: [12, 24, 50, 100],
+                getData: loadProviderData
+            });
         }
-    }
-});
+
+        $state.current.data.tableParams = $scope.tableParams;
+
+
+        function reloadTable() {
+            $scope.tableParams.reload();
+        };
+
+        function select(itemId) {
+            $scope.selectedItemId = itemId;
+        }
+
+
+        function changeFreelance(item) {
+            FreelanceService.changeFreelance(item)
+                .then(function(freelance) {
+                    item.freelance = freelance;
+                    $scope.tableParams.reload();
+                });
+        }
+
+
+        function changeEmployment(item) {
+            EmploymentService.changeEmployment(item)
+                .then(function(employment) {
+                    item.employment = employment;
+                    $scope.tableParams.reload();
+                });
+        }
+
+        function changeType(item, typeId) {
+            ProviderTypeService
+                .changeType(item, typeId)
+                .then(reloadTable);
+        }
+
+
+        $scope.isEmployee = isEmployee;
+
+        function isEmployee(item) {
+            return item.typeId === 0 && item.employment;
+        }
+
+
+        function showAvailabilityCalendar(item) {
+            AvailabilityService.getAvailability(item.id)
+                .then(success, handleHttpError("Не удалось загрузить календарь"));
+
+            function success(eventsource) {
+                var modalInstance = $modal.open({
+                    size: "lg",
+                    resolve: {
+                        eventsource: function() { return eventsource; },
+                        providerId: function() { return item.id; },
+
+                    },
+                    controller: "ProviderAvailabilityCalendarController",
+                    templateUrl: "app/providers/availability/availabilityCalendar/availabilityCalendar.dlg.tpl.html"
+                });
+
+                modalInstance.result.then(reloadTable, reloadTable);
+            }
+        }
+    });
